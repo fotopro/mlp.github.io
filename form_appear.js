39461390547a6372ed81374bda0b0eece18fe7a3
VK30 <59475436+VK30@users.noreply.github.com>
2020-02-07 23:27:29 +0500

    ы

--- form_appear.js
+++ form_appear.js
@@ -1,3 +1,9 @@
   
 function appear(){document.getElementById('form_overlay').style.display='block';
 }
+
+ if (document.documentElement.clientWidth < 480) {
+            document
+                .querySelector("meta[name=viewport]")
+                .setAttribute('content', 'initial-scale=0.3', 'maximum-scale=1');
+        };
